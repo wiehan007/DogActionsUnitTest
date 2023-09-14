942bf051238ca1bc776b975b60100dad1c896099
Wiehan Smalberger <csmalbergerw@gmail.com>
2023-09-14 09:14:52 +0200

    first push

---
+++ app/src/test/java/com/example/dogactionsunittest/ExampleUnitTest.kt
@@ -0,0 +1,46 @@
+package com.example.dogactionsunittest
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+
+    val c1 = Dog()
+
+    fun Test_DogMissing() {
+
+        val expected ="The dog is under your feet"
+        val actual = c1.DogMissing()
+        assertEquals(expected, actual)
+    }
+
+    fun Test_HomeworkAddtion() {
+
+        val expected = 10
+        val actual =c1.HomeworkAddtion(5, 5)
+            assertEquals("The dog is under your feet", 2 + 2)
+    }
+
+    fun Test_Homeworksubtraction() {
+
+        val expected = 0
+        val actual = c1.Homeworksubtraction(5, 5)
+            assertEquals("The dog is under your feet", 2 + 2)
+    }
+    fun Test_HomeworkMultiply() {
+
+        val expected =25
+        val actual = c1.HomeworkMultiply(5, 5)
+            assertEquals("The dog is under your feet", 2 + 2)
+    }
+}
